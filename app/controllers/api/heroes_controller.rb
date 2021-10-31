385fb72ca283402d8616424fde14901e15a68283
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-10-30 22:21:21 -0300

    Valida para Hero name ser único

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -20,7 +20,7 @@ class Api::HeroesController < ApplicationController
 
   # POST /heroes
   def create
-    @hero = Hero.new(hero_params.to_h.merge!({token: @token}))
+    @hero = Hero.new(hero_params.to_h.merge!({ token: @token }))
 
     if @hero.save
       render json: @hero, status: :created, location: api_hero_url(@hero)
