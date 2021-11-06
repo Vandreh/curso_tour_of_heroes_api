0227e92aa7fd3e793d8ea51ab13d86fa28c6462c
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-11-06 01:28:34 -0300

    Adiciona testes no models Hero

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,8 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
+  subject { Hero.new(name: 'Zatanna', token: '1234567890') }
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:token) }
+    it { should validate_length_of(:token).is_at_least(10) }
+    it { should validate_uniqueness_of(:name).scoped_to(:token).case_insensitive }
+    # it 'Cria um hero válido' do
+    #   hero = Hero.new(name: 'Xuxa', token: '1')
+    #   expect(hero).to be_valid
+    # end
+    # it 'Não é valido sem o nome' do
+    #   hero = Hero.new( token: '1')
+    #   expect(hero).not_to be_valid
+    # end
   end
+
+  describe 'scopes' do
+    it '.by_token' do
+      expect(Hero.where(token: '1234567890').to_sql).to eq Hero.by_token('1234567890').to_sql
+    end
+    it '.search' do
+      expect(Hero.where('LOWER(name) LIKE ?', '%wonder wonman%').to_sql).to eq Hero.search('wonder wonman').to_sql
+    end
+    it '.sorted_by_name' do
+      expect(Hero.order(name: :desc).to_sql).to eq Hero.sorted_by_name.to_sql
+    end
+  end
+
 end
