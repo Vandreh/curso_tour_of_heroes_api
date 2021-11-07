5ee59057bc564e96609a64dba6add6a05ad44e9a
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-11-07 01:45:22 -0300

    ALtera busca do hero de term para name

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -7,7 +7,7 @@ class Api::HeroesController < ApplicationController
 
   # GET /heroes
   def index
-    @heroes = Hero.by_token(@token).search(params[:term]).sorted_by_name
+    @heroes = Hero.by_token(@token).search(params[:name]).sorted_by_name
 
     # puts "====> #{params}"
     # p "====> TOKEN: #{@token}"
